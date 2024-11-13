b45f4c87a4580f2d75b51641e0181c8ba57f30d1
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 00:34:59 -0600

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✧ Listo!')
+    m.reply('𖣐 Listo!')
     // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
