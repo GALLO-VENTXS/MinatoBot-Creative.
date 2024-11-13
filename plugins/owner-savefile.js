7c8bec4dc58e8de3ef89092c5f41f3f17d6d8a73
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 00:59:08 -0600

    Actualizar owner-savefile.js

--- plugins/owner-savefile.js
+++ plugins/owner-savefile.js
@@ -1,16 +1,16 @@
 import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '✦ Comando no disponible para subbots', m )
+return conn.reply(m.chat, '*❃ Comando no disponible para subbots*', m )
 }
-if (!text) return m.reply(`✧ Ingresa la Ruta y el nombre del Archivo junto al comando.`)
+if (!text) return m.reply(`𖣐 Ingresa la Ruta y el nombre del Archivo junto al comando.`)
 try {
-if (!m.quoted.text) return m.reply(`✧ Responder al mensaje.`)
+if (!m.quoted.text) return m.reply(`𖣐 Responder al mensaje.`)
 let path = `${text}`
 await fs.writeFileSync(path, m.quoted.text)
 m.reply(`✧ Guardado en *${path}*.`)
 } catch {
-await m.reply(`✧ Responder al mensaje.`)
+await m.reply(`𖣐 Responder al mensaje.`)
 }}
 handler.command = ["savefile", "sf", "savecmd"]
 handler.tags = ['owner']
