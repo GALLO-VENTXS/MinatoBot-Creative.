cefaa5d8d900f6592238e2b1ddd9bab77817d53a
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 01:09:36 -0600

    Actualizar exp-unreg.js

--- plugins/exp-unreg.js
+++ plugins/exp-unreg.js
@@ -1,11 +1,11 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { args }) {
-  if (!args[0]) throw m.reply('*✧ Ingresa tu número de série.*');
+  if (!args[0]) throw m.reply('*⚡ Ingresa tu número de série.*');
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw m.reply('✧ Este no es tu número de série')
   user.registered = false
-  m.reply('```✧ Ya no está registradx!```')
+  m.reply('```𖣐 Ya no está registradx!```')
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN>')
 handler.tags = ['xp']
