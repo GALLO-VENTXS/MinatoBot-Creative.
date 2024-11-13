b8cbe8405b1f6d03a9692193539ba5fefbd5313d
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 13:04:36 -0600

    Actualizar sticker-meme.js

--- plugins/sticker-meme.js
+++ plugins/sticker-meme.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [atas, bawah] = text.split`|`
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw m.reply(`✧ Responde a una *Imagen* con el comando\n\n${usedPrefix + command} <${atas ? atas : 'Texto Arriba'}>|<${bawah ? bawah : 'Texto Bajo'}>`)
+    if (!mime) throw m.reply(`𖣐 Responde a una *Imagen* con el comando\n\n${usedPrefix + command} <${atas ? atas : 'Texto Arriba'}>|<${bawah ? bawah : 'Texto Bajo'}>`)
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let url = await uploadPomf(img)
