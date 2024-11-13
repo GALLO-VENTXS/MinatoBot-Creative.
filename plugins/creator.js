30e71dc7b064b3c3e112a8db86b9317051c1f68b
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-12 22:15:33 -0600

    Actualizar creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,8 +5,8 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `𖣐 Developer Bot `, `No famoso`, `galloventas.20@gmail.com`, `🇦🇷 Argentina`, `📍 https://github.com/GALLO-VENTXS`, `⚡ Owner Minato Creative`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `✧ Whatsapp Bot`, `𖣐 No hagas spam.`, `galloventas.20@gmail.com`, `🇦🇷 Argentina`, `📍 https://github.com/GALLO-VENTXS/MinatoBot-Creative`, `Si hay un error habla con mi owner ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `𖣐 Developer Bot `, `No famoso`, `galloventas.20@gmail.com`, `🇨🇴 Colombia`, `📍 https://github.com/GALLO-VENTXS`, `⚡ Owner Minato Creative`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `✧ Whatsapp Bot`, `𖣐 No hagas spam.`, `galloventas.20@gmail.com`, `🇲🇽 México`, `📍 https://github.com/GALLO-VENTXS/MinatoBot-Creative`, `Si hay un error habla con mi owner ☺`]
   ], fkontak)
   await m.reply(`Hola @${m.sender.split(`@`)[0]} solo habla con mi Owner por temas del bot.`)
   } 
