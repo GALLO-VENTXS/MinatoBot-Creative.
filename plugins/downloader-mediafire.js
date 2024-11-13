9571ff16735289df6408dc188d98bdbcc14d4994
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 14:14:49 -0600

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -5,7 +5,7 @@ if (!text) throw m.reply(`Ingresa un link de mediafire\n*⚡ Ejemplo:* ${usedPre
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
 	let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
   let gyh = await ouh.json()
-	await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*⚡ Nombre:* ${gyh.data[0].nama}\n*✧ Tamaño:* ${gyh.data[0].size}\n*✧ Extensión:* ${gyh.data[0].mime}`, m)
+	await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*⚡ Nombre:* ${gyh.data[0].nama}\n*⚡ Tamaño:* ${gyh.data[0].size}\n*✧ Extensión:* ${gyh.data[0].mime}`, m)
 	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
 }
 handler.help = ['mediafire']
