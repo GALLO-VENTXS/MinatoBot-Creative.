e13fc9140ebc25be3dc4f9e61ac8766393a74142
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 12:58:13 -0600

    Actualizar owner-demote.js

--- plugins/owner-demote.js
+++ plugins/owner-demote.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, participants }) => {
      let user = m.mentionedJid && m.mentionedJid[0]
             await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
         
-    m.reply('✧ Listo')
+    m.reply('⚡ Listo')
 
 }
 handler.help = ['odemote @tag']
