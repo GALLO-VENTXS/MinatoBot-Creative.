0320428d2ef05ef26aa9e0f0d5568fed961eb16d
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 12:54:02 -0600

    Actualizar tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,7 +1,7 @@
 import { toDataURL } from 'qrcode'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, '*✧ Ejemplo:* \n.qrcode Hola', m)
+    if (!text) return conn.reply(m.chat, '*⚡ Ejemplo:* \n.qrcode Hola', m)
 
     conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
