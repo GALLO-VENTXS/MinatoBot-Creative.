0fb732b92cda0446f88482aa938ca4e86dee780c
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-12 22:42:18 -0600

    Actualizar _getpp.js

--- plugins/_getpp.js
+++ plugins/_getpp.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, command }) => {
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
         else who = m.quoted.sender ? m.quoted.sender : m.sender
         let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-        conn.sendFile(m.chat, pp, "nih bang.png", '✧ Listo....', m, { jpegThumbnail: await (await fetch(pp)).buffer() })
+        conn.sendFile(m.chat, pp, "nih bang.png", '⚡ Listo....', m, { jpegThumbnail: await (await fetch(pp)).buffer() })
     } catch {
         let sender = m.sender
         let pp = await conn.profilePictureUrl(sender, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
