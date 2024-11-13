8d641aebcd18135dc1c402660f34b3d7376625c6
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 12:57:43 -0600

    Actualizar rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -1,20 +1,20 @@
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('✧ Ingresa la cantidad de *❇️ Eris* que deseas Retirar.')
+   if (!args[0]) return m.reply('𖣐 Ingresa la cantidad de *❇️ Eris* que deseas Retirar.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
       user.limit += count * 1
-      await m.reply(`✧ Retiraste *${count} ❇️ Eris* del Banco.`)
+      await m.reply(`𖣐 Retiraste *${count} ❇️ Eris* del Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('✧ La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('𖣐 La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
    if (!user.bank) return m.reply('No tienes *❇️ Eris* en el Banco.')
    if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ❇️ Eris* en el Banco.`)
    user.bank -= count * 1
    user.limit += count * 1
-   await m.reply(`✧ Retiraste *${count} ❇️ Eris* del Banco.`)
+   await m.reply(`𖣐 Retiraste *${count} ❇️ Eris* del Banco.`)
 }
 
 handler.help = ['retirar']
