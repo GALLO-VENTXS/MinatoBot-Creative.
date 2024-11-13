bb1498c0f6f4ab3abb4e62ca70ab0b9a59107901
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 00:35:39 -0600

    Actualizar downloader-aio.js

--- plugins/downloader-aio.js
+++ plugins/downloader-aio.js
@@ -4,7 +4,7 @@ import path from 'path';
 import fetch from "node-fetch"
 
 let handler = async (m, { conn, args, text }) => {
-	if (!args[0]) throw m.m.reply('✧ Ingresa un link de:\n- YouTube\n- instagram\n- tiktok\n- facebook\n- twitter ')
+	if (!args[0]) throw m.m.reply('𖣐 Ingresa un link de:\n- YouTube\n- instagram\n- tiktok\n- facebook\n- twitter ')
 	
 await conn.sendMessage(m?.chat, {react: {text: `🌟`, key: m?.key}});
 class Fuck extends Error {
