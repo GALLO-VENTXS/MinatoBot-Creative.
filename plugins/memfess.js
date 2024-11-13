848f9be96f574c61b52399b5581a1fc73a673426
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-12 22:45:08 -0600

    Actualizar memfess.js

--- plugins/memfess.js
+++ plugins/memfess.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     conn.menfess = conn.menfess ? conn.menfess : {}
-    if (!text) throw m.reply(`*✧ Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*✧ Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*✧ Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
+    if (!text) throw m.reply(`*⚡ Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*⚡ Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*⚡ Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
     let [jid, name, pesan] = text.split('|');
-    if ((!jid || !name || !pesan)) throw m.reply(`*✧ Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*✧ Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*✧ Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
+    if ((!jid || !name || !pesan)) throw m.reply(`*⚡ Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*⚡ Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*⚡ Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
     jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
     let data = (await conn.onWhatsApp(jid))[0] || {};
-    if (!data.exists) throw m.reply('✧ El numero no esta registrado en WhatsApp .');
-    if (jid == m.sender) throw m.reply('✧ No puedes mandarte un mensaje a tí mismo.')
+    if (!data.exists) throw m.reply('⚡ El numero no esta registrado en WhatsApp .');
+    if (jid == m.sender) throw m.reply('⚡ No puedes mandarte un mensaje a tí mismo.')
     let mf = Object.values(conn.menfess).find(mf => mf.status === true)
     if (mf) return !0
     	let id = + new Date
