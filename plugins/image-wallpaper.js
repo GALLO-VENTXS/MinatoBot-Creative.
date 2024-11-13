6945380734048a39e75eb1864209993d30879076
GλLLØ VENŦXS <galloventas.20@gmail.com>
2024-11-13 00:53:01 -0600

    Actualizar image-wallpaper.js

--- plugins/image-wallpaper.js
+++ plugins/image-wallpaper.js
@@ -2,12 +2,12 @@ import { wallpaper } from "../lib/scrape.js";
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     let query = text
-    if (!query) throw m.reply(`*✧ Ingresa una consulta. `);
+    if (!query) throw m.reply(`*⚡ Ingresa una consulta. `);
     m.reply(wait);
     try {
         let wallpapers = await wallpaper(query);
         let randomWallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-        let cap = "*✧ Fuente:* wallpaperflare.com";
+        let cap = "*⚡ Fuente:* wallpaperflare.com";
         conn.sendMessage(m.chat, { image: { url: randomWallpaper }, caption: cap }, m);
     } catch (e) {
         console.log(e);
